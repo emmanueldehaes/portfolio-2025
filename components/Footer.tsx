7ddfbaad4642fa90728ceb9d85ea285499a1658d
emmanueldehaes <manudehaes@gmail.com>
2024-06-25 10:22:18 +0200

    fixed bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -12,10 +12,10 @@ const Footer = () => {
           presence to the next level?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
+          Reach out to me today and let's discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@emmanueldehaes.com">
+        <a href="#message">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
@@ -27,15 +27,17 @@ const Footer = () => {
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Emmanuel De Haes
         </p>
-
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
+            <a
               key={info.id}
+              href={info.link}
+              target="_blank"
+              rel="noopener noreferrer"
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            </a>
           ))}
         </div>
       </div>
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -157,6 +157,7 @@ export const BentoGridItem = ({
                     key={i}
                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
                     lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    id="message"
                   >
                     {item}
                   </span>
